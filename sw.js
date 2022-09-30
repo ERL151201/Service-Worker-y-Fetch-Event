3d1d9c666f90a1e3b8dbe18dc5bcb7279a507208
Emanuel Rangel <emanuelrangelluna@gmail.com>
2022-09-30 09:13:34 -0500

    5.-manejo de errores ene el fecth event

--- sw.js
+++ sw.js
@@ -1,9 +1,13 @@
 self.addEventListener('fetch', event => {
 
-    if (event.request.url.includes('main.jpg')) {
 
-        let resp = fetch('img/main-patas-arriba.jpg');
-        event.respondWith(resp);
+    const resp =
+        fetch(event.request)
+        .then(resp => {
+            return resp.ok ? resp : fetch('img/main.jpg');
+        });
+    event.respondWith(
+        resp
+    );
 
-    }
 });
