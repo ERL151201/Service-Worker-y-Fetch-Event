5cf3723a89719cf54bf1975d9c2e3fec2437bb13
Emanuel Rangel <emanuelrangelluna@gmail.com>
2022-09-29 22:35:29 -0500

    4.- tarea interceptar y modificar oeticines

--- sw.js
+++ sw.js
@@ -1,19 +1,9 @@
 self.addEventListener('fetch', event => {
 
-    if (event.request.url.includes('style.css')) {
-        let respuesta = new Response(`
-            body{
-                background-color: red !important;
-                color: pink;
-            }
-        `, {
-            headers: {
-                'Content-Type': 'text/css'
-            }
-        });
+    if (event.request.url.includes('main.jpg')) {
 
-        event.respondWith(respuesta);
+        let resp = fetch('img/main-patas-arriba.jpg');
+        event.respondWith(resp);
+
     }
-
-
 });
